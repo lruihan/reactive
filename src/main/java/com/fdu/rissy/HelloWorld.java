e6edb7a8e88817f55b6bd309f5f7e50f08aa745c
Rissy Lin <lrissy@vmware.com>
2017-09-06 16:00:45 +0800

    Reactive Programming
    RxJava2 first example

---
+++ src/main/java/com/fdu/rissy/HelloWorld.java
@@ -0,0 +1,42 @@
+package com.fdu.rissy;
+
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Flowable.just("Hello World").subscribe(System.out::println);
+
+        Flowable.fromCallable(() -> {
+            Thread.sleep(1000);
+            return "Done";
+        }).subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.single())
+                .subscribe(System.out::println, Throwable::printStackTrace);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Flowable.range(1, 10)
+                .observeOn(Schedulers.computation())
+                .map(v -> v * v)
+                .blockingSubscribe(System.out::println);
+
+        Flowable.range(1, 10)
+                .flatMap(v ->
+                    Flowable.just(v)
+                        .subscribeOn(Schedulers.computation())
+                        .map(w -> w * w))
+                .blockingSubscribe(System.out::println);
+          Flowable.range(1, 10)
+                  .parallel()
+                  .runOn(Schedulers.computation())
+                  .map(v -> v * v)
+                  .sequential()
+                  .blockingSubscribe(System.out::println);
+    }
+}
